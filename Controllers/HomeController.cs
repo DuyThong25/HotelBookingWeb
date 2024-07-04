3e00df90cda9d32f26b3c720b6e99a244677d7ba
duythong2505@gmail.com <duythong2505@gmail.com>
2024-07-04 13:14:27 +0700

    UI for home page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,20 +7,44 @@ namespace HotelBookingWeb.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly HttpClient _httpClient;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, HttpClient httpClient)
         {
             _logger = logger;
+            _httpClient = httpClient;
         }
 
         public IActionResult Index()
         {
+
             return View();
         }
 
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetDataProvince()
+        {
+            // Replace "API_URL" with the actual URL of the third-party API you want to access
+            var apiUrl = "https://vn-public-apis.fpo.vn/provinces/getAll?limit=-1";
+
+            HttpResponseMessage response = await _httpClient.GetAsync(apiUrl);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string data = await response.Content.ReadAsStringAsync();
+                // Process the data or return it as-is
+                return Json(new {data = data});
+            }
+            else
+            {
+                // Handle the error condition
+                return StatusCode((int)response.StatusCode);
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
